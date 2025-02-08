3acdaf0e18a53759cbbbff9a5ed6785fdc68ff05
Liam's Apps <liams.systems@gmail.com>
2025-02-08 03:10:05 -0500

    Add button to allow user to set grid size
    Clicking the button will prompt the user to enter the number of
    squares, per side, for the new grid. This will be available on page
    load, after the default grid (16x16) has already been created via a
    newly added page load event (DOMContentLoaded) that ensures the default
    grid (16x16) is created and available to the user when arriving on
    the site.
    
    If the user chooses, he can create a new grid or use the default grid.
    Regardless of the user's choice, the following functionality will be
    available:
    
    As the user moves their mouse over a div (mouseenter), it's background
    color will change. When leaving the div's boundaries (mouseleave), it's
    background color will remain the same. This will appear as a pixelated
    trail through the grid like a pen would on an 'Etch-A-Sketch'.

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -5,14 +5,37 @@
  * Description: Create a browser based 'Etch A Sketch' toy using Javascript.
  */
 
-const container = document.querySelector("#container");
-
 let squaresPerRow = 16;
 let squares = squaresPerRow ** 2;
 let squareWidth = 480 / squaresPerRow;
-let numRows = 480 / squareWidth;
 
-// console.log(`${squares} squares each having a width of ${squareWidth} over ${numRows} rows`);
+const container = document.querySelector("#container");
+const btnCreateGrid = document.createElement("button");
+btnCreateGrid.textContent = "Create Grid";
+
+document.addEventListener("DOMContentLoaded", (e) => {
+    console.log(e);
+    createGrid(squares, squareWidth);
+});
+
+btnCreateGrid.addEventListener("click", () => {
+    let rowSize = 0;
+    do { 
+        rowSize = prompt("Enter the number of squares per side for the new grid\nMust be a whole positive number not greater than 64");
+        // console.log(rowSize);
+        let squaresPerRow = rowSize;
+        let squares = squaresPerRow ** 2;
+        let squareWidth = 480 / squaresPerRow;
+
+        createGrid(squares, squareWidth);
+    
+    } while (!(Number.isInteger(+rowSize)) || rowSize < 1 || rowSize > 64 || isNaN(rowSize))
+    
+});
+
+// have button at top of page
+let parent = container.parentNode;
+parent.insertBefore(btnCreateGrid, container);
 
 // generate a random list of 10 hexadecimal colors
 function generateColorList() {
@@ -32,7 +55,8 @@ function getColorFromList() {
     return colorList[Math.floor(Math.random() * colorList.length)];
 }
 
-// will be used to set a random background color on an active (hovering) div
+// will be used to set a random background color on an active (hovering) div,
+// will also do the same as mouse leaves div 
 function getCssRules() {
     var rules = {};
     for (var i = 0; i < document.styleSheets.length; ++i) {
@@ -50,41 +74,48 @@ function cssGetClass(name) {
     return rules[name];
 }
 
-// create a box of square div elements
-for (i = 0; i < squares; i++) {
-    let square = document.createElement("div");
-    // set each div's id (makes div easier to access)
-    square.setAttribute("id", `square${i}`);
-    // set width based on number of divs per row
-    square.style.width = `${squareWidth}px`;
+function createGrid(squares, squareWidth) {
+
+    const gridDivs = document.querySelector("#container");
+    if (gridDivs.hasChildNodes()) {
+        console.log('children found');
+        gridDivs.replaceChildren();
+        console.log('container cleared');
+    }
+    else {
+        console.log('first time here');
+    }
     
-    // fires when cursor enters each div element's boundaries
-    square.addEventListener("mouseenter", (e) => {
-        // retrieve event target's id
-        let divTargetID = e.target.id;
-        console.log(`Before: ${divTargetID}`);
-        let divEntered = document.querySelector(`#${divTargetID}`);
-
-        // change background-color in .entered CSS class
-        // WORKS
-        // cssGetClass('.entered').style.background = "pink";
-        cssGetClass('.entered').style.background = getColorFromList();
-
-        divEntered.classList.add("entered");
-
-    });
-
-    // fires when cursor is over the div element and then moves to the outside of the div's boundaries
-    square.addEventListener("mouseleave", (e) => {
-        // retrieve event source element's id
-        let divTargetID = e.srcElement.id;
-        let divExited = document.querySelector(`#${divTargetID}`);
-        // deactivate the class to remove background color
-        divExited.classList.toggle("entered");
+    // create a box of square div elements
+    for (i = 0; i < squares; i++) {
+        let square = document.createElement("div");
+        // set each div's id (makes div easier to access)
+        square.setAttribute("id", `square${i}`);
+        // set width based on number of divs per row
+        square.style.width = `${squareWidth}px`;
         
-        
-    });
+        // fires when cursor enters each div element's boundaries
+        square.addEventListener("mouseenter", (e) => {
+            // retrieve event target's id
+            let divTargetID = e.target.id;
+            console.log(`Before: ${divTargetID}`);
+            let divEntered = document.querySelector(`#${divTargetID}`);
 
-    
-    container.appendChild(square);
+            // using CSS background-color: blue
+            divEntered.classList.add("entered");
+
+        });
+
+        // fires when cursor is over the div element and then moves to the outside of the div's boundaries
+        square.addEventListener("mouseleave", (e) => {
+            // retrieve event source element's id
+            let divTargetID = e.srcElement.id;
+            let divExited = document.querySelector(`#${divTargetID}`);
+
+            // nothing to do here when using single background color
+        });
+
+        
+        container.appendChild(square);
+    }
 }
